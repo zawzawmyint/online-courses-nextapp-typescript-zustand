a12958324747f54e9c598cdac4a2f69781085b1a
Zaw Zaw Myint <cuzawzawmyint@gmail.com>
2024-08-16 18:38:41 +0800

    [update] herosection

--- src/app/(root)/courses/[id]/page.tsx
+++ src/app/(root)/courses/[id]/page.tsx
@@ -13,7 +13,7 @@ const page = ({ params }: { params: { id: string } }) => {
           <CourseDetails id={params.id} />
         </Suspense>
       </BaseContainer>
-      <HeroSection position="bottom-32">
+      <HeroSection position="bottom-44">
         <Tipsection id={params.id} />
       </HeroSection>
     </div>
--- src/components/global/container/BaseContainer.tsx
+++ src/components/global/container/BaseContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BaseContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="p-2 bg-background rounded-lg z-10 transition-all duration-500">
+    <div className="p-2 bg-background rounded z-10 ">
       <div className="max-w-7xl mx-auto">{children}</div>
     </div>
   );
